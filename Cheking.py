d915936036713ca94b1cff722b9597ce00b5de57
Hoàng Linh <84447084+HoangLinh03-code@users.noreply.github.com>
2025-10-24 12:27:25 +0700

    Load environment variables based on execution context

--- Cheking.py
+++ Cheking.py
@@ -19,6 +19,13 @@ from PyQt5.QtWidgets import (
 from PyQt5.QtGui import QFont
 from api.callApi import get_credentials
 from process.CheckThread import CheckThread
+if getattr(sys, 'frozen', False):
+    base_path = sys._MEIPASS
+else:
+    base_path = os.path.dirname(__file__)
+
+dotenv_path = os.path.join(base_path, '.env')
+load_dotenv(dotenv_path)
 # ==================== MAIN WINDOW (giữ nguyên UI cũ) ====================
 class MainWindow(QWidget):
     def __init__(self):
